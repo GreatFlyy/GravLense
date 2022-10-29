5318868448180e9b856574ccfa492e08e7056300
Гардынский Инокентий <inokgard78@mail.ru>
2022-10-29 14:08:21 +0300

    pointmass

--- GravLense/main.cpp
+++ GravLense/main.cpp
@@ -7,10 +7,12 @@
 
 int main()
 {
-	string pathsource = "source.bmp"; //пути к файлам
+	string pathsource = "source.bmp";
+	string pathimage = "image.bmp"; //пути к файлам
 
-	read_bmp rb(pathsource);
-	bmp source(rb.read()); //открытие и чтение картинки источника
+	read_bmp rbi(pathsource);
+	bmp source(rbi.read());
+	rbi.close(); //открытие и чтение картинки источника
 
 	const double c = 299792458;
 	const double G = 6.67430 * pow(10., -11);
@@ -25,7 +27,7 @@ int main()
 
 	double tethE = 1;
 
-	double scale = 40; //пикс. на радиан
+	double scale = 20; //пикс. на радиан
 
 	double rx = 0;
 	double ry = 0;
@@ -34,6 +36,8 @@ int main()
 
 	double x = 0, y = 0;
 	int x0 = 64, y0 = 64;
+	vector<vector<BYTE>> vals(4, vector<BYTE>(3, 0));
+	
 
 	for (int i = 0; i < image.widthpx; i++)
 	{
@@ -41,15 +45,45 @@ int main()
 		{
 			x = i - x0;
 			y = j - y0;
-			rx = sqrt(x * x + y * y) / (scale * tethE);
-			ry = rx + 1 / rx;
+			rx = sqrt(x * x + y * y) / (scale * tethE); //находим икс из формулы
+			ry = rx - 1 / rx; //сама формула
 
-			x = -x * ry / rx;
-			y = -y * ry / rx;
+			x = x * ry / rx; //нахождние координат на сурсе (не домножаю на коеффы, т.к. отношение)
+			y = y * ry / rx;
+
+			x += x0; //привод к кордам картинки
+			y += y0;
+
+			if (x >= image.widthpx - 1 || y >= image.heightpx - 1 || x <= 0 || y <= 0 || isnan(x) || isnan(y)) //проверка попадает ли наши корды в сурс
+			{
+				vals = vector<vector<BYTE>>(4, vector<BYTE>(3, 0)); //если в сурс не попал то очевидно 0
+				for (int k = 0; k < 3; k++)
+				{
+					image.pixarr[i][j][k] = 0;
+				}
+			}
+			else
+			{
+				vals[0] = source.pixarr[(int)x][(int)y]; //сохранение значений пикселей для интерполяции
+				vals[1] = source.pixarr[(int)x][(int)y + 1];
+				vals[2] = source.pixarr[(int)x + 1][(int)y];
+				vals[3] = source.pixarr[(int)x + 1][(int)y + 1];
 
 
+				x = x - (int)x;
+				y = y - (int)y;
+
+				for (int k = 0; k < 3; k++) //собственно интерполяция
+				{
+					image.pixarr[i][j][k] = vals[0][k] * (1 - x - y + x * y) + vals[1][k] * (y - x * y) + vals[2][k] * (x - x * y) + vals[3][k] * (x * y);
+				}
+			}
 		}
 	}
+
+	read_bmp rbo(pathimage);
+	rbo.print(image);
+	rbo.close();
 
 
 
